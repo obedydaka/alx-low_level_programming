349cc99822d2fafdd8a3c7d64cb21a37524f0684
Obedy Daka <obedydaka01@gmail.com>
2023-02-17 09:37:39 -0800

    Printting in UNIX with standard error 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 
 /**
  * main - Entry point
- * Return: Always 1
+ *
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\,\n");
 	
 	return (1);
 }
