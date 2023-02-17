851df3f88bebc0a0bd1613c6e5fed138f0850682
Obedy Daka <obedydaka01@gmail.com>
2023-02-17 06:34:45 -0800

    Printing in printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,
+ * followed by a new line.
+ * Return: always 0.
+ */
+
+int main(void)
+
+{
+	printf("\"with proper grammar, but the out come is a piece of art,"
+
+	return (0);
+}
