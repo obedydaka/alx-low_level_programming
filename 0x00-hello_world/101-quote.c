473b774a2d7ff59ac636de1f77cebd0927979a2e
Obedy Daka <obedydaka01@gmail.com>
2023-02-17 09:29:43 -0800

    Printting in UNIX with standard error 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
 
 int main(void)
 {
-	printf("and that piece of art is useful""");
-	printf("- Dora Korpar, 2015-10-19\n");
-	
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
