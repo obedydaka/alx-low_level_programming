24ef768fceedfd4d00dee93231ae19cb5b3f1bd4
Obedy Daka <obedydaka01@gmail.com>
2023-02-17 15:06:26 -0800

    Print numbers in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Begin
+ * Description
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
