1c45c78e1a6e711521271d9f4c0dca96575f1aba
Obedy Daka <obedydaka01@gmail.com>
2023-02-17 13:51:05 -0800

    Listing letters a to z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - begin
+ * Description
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char llt;
+
+	for (llt = 'a'; llt <= 'z'; llt++)
+		putchar (llt);
+		putchar ('\n');
+
+	return (0);	
