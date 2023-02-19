007843a2cb4d59341cd7937c3c96a914c132830a
Obedy Daka <obedydaka01@gmail.com>
2023-02-19 14:49:13 -0800

    Printing a sequence of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Begin code
+ * Description
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, 
+	int n;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (n = i + 1; n <= '9'; n++)
+		{
+			if (n != i)
+			{
+				putchar(i);
+				putchar(n);
+
+				if (i == '8' && n == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
