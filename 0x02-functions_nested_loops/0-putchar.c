7b430e8d14b20176ff7846568e838c71e1a96f2c
Obedy Daka <obedydaka01@gmail.com>
2023-02-21 13:25:00 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Using 'putchar function'
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+
+	Return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is about C programmes in loops, nested loops and functions
