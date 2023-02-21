90dda9df533cd27bd8b1e83d47a4a16043e5515f
Obedy Daka <obedydaka01@gmail.com>
2023-02-21 14:36:01 -0800

    Priting lowercase alphabet 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * print_alphabet - Check description below.
- * Description: Printing alphabetical order in lowercase fallowed by a new line.
+ * Description: Printing alphabet in lowercase fallowed by a new line.
  * Return: Nothing.
  */
 
