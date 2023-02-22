c77b13ce466befe4c4b611c43fb0f36155a48a56
Obedy Daka <obedydaka01@gmail.com>
2023-02-22 06:47:19 -0800

    Found letter!

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Check main.h
+ * @c: Input a character
+ * Description: This function returns 1 if the input character is a
+ * lowercase or uppercase alphabetic letter.
+ * Return: 1 or 0 in otherwise.
+ */
+
+int _isalpha(int c)
+{
+	char lowerc;
+	int upperc;
+	int foundC = 0;
+
+	for (lowerc = 'a'; lowerc <= 'z'; lowerc++)
+	{
+		for (upperc = 'A'; upperc <= 'Z'; upperc++)
+		{
+			if (c == lowerc || c == upperc)
+			{
+				foundC = 1;
+			}
+		}
+	}
+	return (foundC);
+}
