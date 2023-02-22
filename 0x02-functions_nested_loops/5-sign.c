6a6cd5cee6a3c2fe16bef47757c233696e67986a
Obedy Daka <obedydaka01@gmail.com>
2023-02-22 08:42:42 -0800

    Getting the absolute value

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 
 /**
  * _sign - check main
- * @x: an input character
+ * @n: an input character
  * Description: This program checks whether
  * the number is negative or positive.
  * Return: 1 if number is positive, 0 is number is 0 
  * or -1 if number is negative
  */
 
-void _sign(int x)
+void print_sign(int n)
 {
 	int number;
 
-	if (x > 0)
+	if (n > 0)
 	{
 		number = 1;
 		_putchar('+');
 	}
-	else if (x == 0)
+	else if (n == 0)
 	{
 		number = 0;
 		_putchar('0');
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_abs - Check main
+ * @r: Input interger number
+ * Description: This function returns absolute value of 
+ * any given number
+ * Return: Absolute value of the number r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
