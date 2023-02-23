bb37586e7e6bf6e1a36ffbba8a71348287cbdffc
Obedy Daka <obedydaka01@gmail.com>
2023-02-23 04:11:39 -0800

    Finding highest negative and positive numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,6 +12,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a >= b && b >= c)
 	{
 		largest = a;
@@ -20,7 +21,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c >= a && c >= b)
 	{
 		largest = c;
 	}
