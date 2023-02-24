db898afd48b80c202b6244eadfa607004ba72567
Obedy Daka <obedydaka01@gmail.com>
2023-02-24 01:54:51 -0800

    Revisiting errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,14 +12,11 @@ void print_most_numbers(void)
 
 	for (; i <= '9'; i++)
 	{
-		if (i != '2' || i != '4')
-		{
-			_putchar(i);
-		}
-		else
+		if (i == '2' || i == '4')
 		{
 			continue;
 		}
+		_putchar(i)
 	}
 	_putchar('\n');
 }
