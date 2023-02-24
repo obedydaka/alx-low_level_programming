32adb347cd368c9664ecdd1ecc3d68bdaabd5cdd
Obedy Daka <obedydaka01@gmail.com>
2023-02-24 05:14:17 -0800

    New add 3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,28 @@
 
 int main(void)
 {
-	int i = 1;
-
-	for (; i < 100 ; i++)
+	int i;
+	
+	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
+		if (i != 0)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+				continue;
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+				continue;
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+				continue;
+			}
 			printf("%d ", i);
+		}
 	}
-	printf("Buzz\n");
-
-	return (0);
 }
