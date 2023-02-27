b9d5403f718316ba81a0fe00ee3ec2cf7c86b0e1
Obedy Daka <obedydaka01@gmail.com>
2023-02-27 12:28:45 -0800

    Second task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/** void swap_int: This function swaps the input of 
+ * two intergers pointers
+ * @a: First interger pointer input
+ * @b: Second interger pointer input
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swp;
+
+	swp = *a;
+	*a = *b;
+	*b swp;
+}
