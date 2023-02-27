59663b5723381f49f975c9a43e8f93aabeb32790
Obedy Daka <obedydaka01@gmail.com>
2023-02-27 10:19:32 -0800

    First submit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 : This fuction takes any int parameter
+ * and updates it's value to 98
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n);
+{
+	int *ptr;
+
+	ptr = n;
+	ptr = 98;
+}
