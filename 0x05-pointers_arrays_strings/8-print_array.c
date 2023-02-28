182021815a221007896080116dfb1d95511b47d2
Obedy Daka <obedydaka01@gmail.com>
2023-02-28 15:07:27 -0800

    Third edit 3

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,8 +15,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		printf("%d, ", a[i]);
-		i++;
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
 	}
 	_putchar('\n');
 }
