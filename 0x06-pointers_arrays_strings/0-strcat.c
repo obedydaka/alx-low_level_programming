93a83aa444cb7c95ec21ab190d6ceae3242fc3be
Obedy Daka <obedydaka01@gmail.com>
2023-03-01 10:24:31 -0800

    First entry

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - This function concatenates 2 strings
+ * @dest: This is a destination string
+ * @src: This is a source string
+ * Return: A character value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest , src);
+	return (dest);
+}
