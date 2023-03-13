cf0747be306f8f9729b9425b55a4845ea731b4b8
Obedy Daka <obedydaka01@gmail.com>
2023-03-13 07:01:06 -0700

    Entry 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints it's name
+ * @argc: Arguments count
+ * @argv: Argument array
+ * Return: return 0 (SUCCESS)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
