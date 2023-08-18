0653547becca386e156d10b1e861eb892d00ba39
Obedy Daka <obedydaka01@gmail.com>
2023-08-18 09:30:41 -0700

    Adjusting task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,32 +10,32 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *the_new_node = NULL;
+	dlistint_t *new_node = NULL;
 	*temp = NULL;
 
-	the_new_node = malloc(sizeof(dlistint_t));
-	if (!the_new_node)
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
 	{
-		free(the_new_node);
+		free(new_node);
 		return (NULL);
 	}
 
-	the_new_node->next = NULL;
-	the_new_node->prev = NULL;
-	the_new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
 
 	if (!head || !(*head))
 	{
-		*head = the_new_node;
-		return (the_new_node);
+		*head = new_node;
+		return (new_node);
 	}
 	else
 	{
 		temp = *head;
 		temp->prev = new_node;
-		*head = the_new_node;
-		the_new_node->next = temp;
-		return (the_new_node);
+		*head = new_node;
+		new_node->next = temp;
+		return (new_node);
 	}
 	return (NULL);
 }
